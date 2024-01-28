b3e8f74d0fc9732ab41f3741a1d50e75b457ff93
Anastasia <anastasija2003@mail.ru>
2024-01-28 15:53:47 +0300

    JC3

---
+++ src/ru/shum/SalaryComparator.java
@@ -0,0 +1,18 @@
+package ru.shum;
+import java.util.Comparator;
+public class SalaryComparator implements Comparator<Employee>{
+    @Override
+    public int compare(Employee emp1, Employee emp2) {
+        double salary1 = emp1.getSalary();
+        double salary2 = emp2.getSalary();
+        // Сравнение зарплат сотрудников
+        if (salary1 < salary2) {
+            return -1;
+        } else if (salary1 > salary2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
